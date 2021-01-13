0c3a322b11d6774bf0d811ae2fa41a9d47f5e738
Clay Ewing <c.ewing@miami.edu>
2021-01-13 11:39:29 -0500

    add allow origin from games.nerdlab.miami

--- play.php
+++ play.php
@@ -1,4 +1,5 @@
 <?php
+	header('Access-Control-Allow-Origin: https://games.nerdlab.miami', false);
 	require 'database.php';
 	error_reporting(0);
 	$id = $_POST['id'];
--- welcome.php
+++ welcome.php
@@ -1,4 +1,7 @@
 <?php
+		
+//Set Access-Control-Allow-Origin with PHP
+header('Access-Control-Allow-Origin: https://games.nerdlab.miami', false);
 	require 'database.php';
 	error_reporting(0);
 	$pid = $_POST['pid'];
